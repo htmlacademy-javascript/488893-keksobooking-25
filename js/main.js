94aa6445c0af84621e1a275820be5323eedfb6fb
Suslov Ivan <ioankingisepp@mail.ru>
2022-02-27 11:10:46 +0300

    Fixed getRandom functions

--- js/main.js
+++ js/main.js
@@ -2,18 +2,16 @@
 * Функция, возвращающая случайное целое число из переданного диапазона включительно.
 * источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 *
-* @param {number} min - Минимальное значение диапазона.
-* @param {number} max - Максимальное значение диапазона.
+* @param {number} a - Значение диапазона (либо минимальное либо максимальное).
+* @param {number} b - Значение диапазона (либо минимальное либо максимальное).
 * @return {number} - Целое число
 */
-function getRandomNumber(min, max) {
-  if (min >= max) {
-    return 0;
-  }
-  min = Math.ceil(min) <= 0 ? 0 : Math.ceil(min);
-  max = Math.floor(max) <= 0 ? 0 : Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const getRandomNumber = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
 
 /**
 * Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
@@ -23,15 +21,12 @@ function getRandomNumber(min, max) {
 * @param {number} decimalPlaces - Число знаков после зяпятой.
 * @return {number} - Число с плавающей точкой
 */
-function getRandomFloat(min, max, decimalPlaces = 1) {
-  if (min >= max) {
-    return 0;
-  }
-  min = min <= 0 ? 0 : min;
-  max = max <= 0 ? 0 : max;
-  const randomFoat = ((Math.random() * (max - min + 1)) + min);
-  return randomFoat.toFixed(decimalPlaces);
-}
+const getRandomFloat = (a, b, decimalPlaces = 1) => {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(decimalPlaces);
+};
 
 //Вызов функции для исключения ошибки esLint
 getRandomNumber(1, 10);
