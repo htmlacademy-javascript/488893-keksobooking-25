5c7bc65765d11c65b84dc0a99d2bdd35834e2b5e
Suslov Ivan <ioankingisepp@mail.ru>
2022-03-14 17:11:49 +0300

    Отрисуй меня полностью (Часть 2)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 import {similarAds} from './data.js';
 import {renderCard} from './card.js';
+import {setInactivePage, setActivePage} from './form.js';
 
 const mapCanvas = document.querySelector('#map-canvas');
 const cards = renderCard(similarAds[0]);
 
 mapCanvas.appendChild(cards);
+setInactivePage();
+setActivePage();
