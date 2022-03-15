8a45ca0c24e4c143472072e6ff974b66fc0b9000
Suslov Ivan <ioankingisepp@mail.ru>
2022-03-15 18:14:35 +0300

    Add validation module and functions.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {similarAds} from './data.js';
 import {renderCard} from './card.js';
-import {setInactivePage, setActivePage} from './form.js';
+import {setInactivePage, setActivePage} from './page-state.js';
+import './form.js';
 
 const mapCanvas = document.querySelector('#map-canvas');
 const cards = renderCard(similarAds[0]);
