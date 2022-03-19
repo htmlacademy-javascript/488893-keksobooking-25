1372d683802c8faf642b191e1b817bda1b35a7be
Suslov Ivan <ioankingisepp@mail.ru>
2022-03-19 19:01:27 +0300

    Add leaflet map and markers

--- js/main.js
+++ js/main.js
@@ -1,11 +1,2 @@
-import {similarAds} from './data.js';
-import {renderCard} from './card.js';
-import {setInactivePage, setActivePage} from './page-state.js';
 import './form.js';
-
-const mapCanvas = document.querySelector('#map-canvas');
-const cards = renderCard(similarAds[0]);
-
-mapCanvas.appendChild(cards);
-setInactivePage();
-setActivePage();
+import './map.js';
--- js/map.js
+++ js/map.js
@@ -1,2 +1,86 @@
-// Модуль работы с картой
-// Пункты ТЗ: 4.
+import {setInactivePage, setActivePage} from './page-state.js';
+import {similarAds} from './data.js';
+import {renderCard} from './card.js';
+
+const address = document.querySelector('#address');
+const resetButton = document.querySelector('.ad-form__reset');
+
+setInactivePage();
+
+const map = L.map('map-canvas')
+  .on('load', () => {
+    setActivePage();
+  })
+  .setView({
+    lat: 35.6574,
+    lng: 139.7785,
+  }, 10)
+  .setZoom(12.45);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: '/img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const mainPinMarker = L.marker(
+  {
+    lat: 35.6574,
+    lng: 139.7785,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  }
+);
+
+mainPinMarker.addTo(map);
+
+mainPinMarker.on('moveend', (evt) => {
+  const coordinates = evt.target.getLatLng();
+  address.value = `${coordinates.lat.toFixed(5)}, ${coordinates.lng.toFixed(5)}`;
+});
+
+resetButton.addEventListener('click', () => {
+  mainPinMarker.setLatLng({
+    lat: 35.6574,
+    lng: 139.7785,
+  });
+
+  map.setView({
+    lat: 35.6574,
+    lng: 139.7785,
+  }, 10)
+    .setZoom(12.45);
+});
+
+const markerIcon = L.icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+
+similarAds.forEach(({location, offer, author}) => {
+  const lat = location.lat;
+  const lng = location.lng;
+  const marker = L.marker(
+    {
+      lat,
+      lng,
+    },
+    {
+      markerIcon,
+    }
+  );
+
+  marker
+    .addTo(map)
+    .bindPopup(renderCard(author, offer));
+});
